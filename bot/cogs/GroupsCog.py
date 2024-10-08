f74154426876cc403e572094252ac33913e9751f
Morrisvdb <morrisvandenbergh@gmail.com>
2024-10-08 15:09:46 +0200

    Added more group commands Added invites and private groups

--- bot/cogs/GroupsCog.py
+++ bot/cogs/GroupsCog.py
@@ -1,9 +1,11 @@
+from operator import is_
+from select import select
 from tokenize import group
 import discord
 from discord.ext import commands
 from sqlalchemy import desc
 from ui.SessionModals import SessionSelectView
-from ui.groupModals import GroupBrowseView, GroupSelectView, GroupEditView
+from ui.groupModals import GroupBrowseView, GroupSelectView, GroupEditView, ViewInvitesView, GroupsViewView
 from models import Session, Group, GroupJoin
 from __init__ import db
 
@@ -15,7 +17,7 @@ class groupsCog(commands.Cog):
     groups_command_group = discord.SlashCommandGroup(name="group", help="Group management commands.", guild_ids=[977513866097479760])
     
     @groups_command_group.command(name="create", description="Create a new group.")
-    async def group_create(self, ctx, name):
+    async def group_create(self, ctx, name, description: discord.Option(str, min=4, max=1023, required=False), private: bool = False):
         selectSessionEmbed = discord.Embed(
             title="Select a session",
             description="Select a session to create a group for.",
@@ -34,7 +36,7 @@ class groupsCog(commands.Cog):
         if session is None:
             await msg.edit(content="Invalid session selected.", view=None, embed=None)
             return
-        group = Group(name=name, owner_id=ctx.author.id, session_id=session.id)
+        group = Group(name=name, owner_id=ctx.author.id, session_id=session.id, private=private)
         db.add(group)
         db.commit()
         
@@ -100,7 +102,7 @@ class groupsCog(commands.Cog):
             await msg.edit(content="Invalid session selected.", view=None, embed=None)
             return
         
-        groups = db.query(Group).filter(Group.session_id == session.id).all()
+        groups = db.query(Group).filter(Group.session_id == session.id, Group.private == False).all()
         if len(groups) == 0:
             await msg.edit(content="No groups found.", view=None, embed=None)
             return
@@ -170,7 +172,7 @@ class groupsCog(commands.Cog):
             color=discord.Color.blue()
         )
         groupSelectView = GroupSelectView(groups)
-        msg = await ctx.respond(embed=groupSelectEmbed, view=groupSelectView, ephemeral=True)
+        await msg.edit(embed=groupSelectEmbed, view=groupSelectView)
         await groupSelectView.wait()
         if groupSelectView.selected_group is None:
             await msg.edit(content="No group selected.", view=None, embed=None)
@@ -250,11 +252,13 @@ class groupsCog(commands.Cog):
         await msg.edit(embed=groupEditEmbed, view=groupEditView)
         await groupEditView.wait()
         change = False
-        if groupEditView.name_is_edited or groupEditView.description_is_edited:
-            if groupEditView.name_is_edited and groupEditView.name is not None:
+        if groupEditView.name_is_edited or groupEditView.description_is_edited or groupEditView.private_is_edited:
+            if groupEditView.name_is_edited and groupEditView.name and groupEditView.private_is_edited is not None:
                 group.name = groupEditView.name
             if groupEditView.description_is_edited and groupEditView.description is not None:
                 group.description = groupEditView.description
+            if groupEditView.private_is_edited and groupEditView.private is not None:
+                group.private = groupEditView.private
             db.add(group)
             db.commit()
             change = True
@@ -264,5 +268,257 @@ class groupsCog(commands.Cog):
         else:
             await msg.edit(content="No changes were made, nothing happened!", view=None, embed=None)
             
+    @groups_command_group.command(name="transfer", description="Transfer ownership of a group.")
+    async def group_transfer(self, ctx, user: discord.Member):
+        if user == ctx.author:
+            await ctx.respond("You silly goose! You can't transfer the group to yourself!", ephemeral=True)
+            return
+        sessionSelectEmbed = discord.Embed(
+            title="Select a session",
+            description="Select a session to pick a group for.",
+            color=discord.Color.blue()
+        )
+        sessionSelectView = SessionSelectView(ctx)
+        msg = await ctx.respond(embed=sessionSelectEmbed, view=sessionSelectView, ephemeral=True)
+        await sessionSelectView.wait()
+        if sessionSelectView.session is None:
+            await msg.edit(content="No session selected.", view=None, embed=None)
+            return
+        
+        session = db.query(Session).filter(Session.id == sessionSelectView.session).first()
+        if session is None:
+            await msg.edit(content="Invalid session selected.", view=None, embed=None)
+            return
+    
+        groups = db.query(Group).filter(Group.owner_id == ctx.author.id, Group.session_id == session.id).all()
+        if len(groups) == 0:
+            await msg.edit(content="No groups found.", view=None, embed=None)
+            return
+        
+        groupSelectEmbed = discord.Embed(
+            title="Select a group",
+            description="Select a group.",
+            color=discord.Color.blue()
+        )
+        groupSelectView = GroupSelectView(groups)
+        await msg.edit(embed=groupSelectEmbed, view=groupSelectView, )
+        await groupSelectView.wait()
+        if groupSelectView.selected_group is None:
+            await msg.edit(content="No group selected.", view=None, embed=None)
+            return
+        
+        group = db.query(Group).filter(Group.id == groupSelectView.selected_group).first()
+        if group is None:
+            await msg.edit(content="Invalid group selected.", view=None, embed=None)
+            return
+        
+        group.owner_id = user.id
+        db.add(group)
+        db.commit()
+        
+    @groups_command_group.command(name="kick", description="Kick a user from a group.")
+    async def group_kick(self, ctx, user: discord.Member):
+        sessionSelectEmbed = discord.Embed(
+            title="Select a session",
+            description="Select a session to pick a group for.",
+            color=discord.Color.blue()
+        )
+        sessionSelectView = SessionSelectView(ctx)
+        msg = await ctx.respond(embed=sessionSelectEmbed, view=sessionSelectView, ephemeral=True)
+        await sessionSelectView.wait()
+        if sessionSelectView.session is None:
+            await msg.edit(content="No session selected.", view=None, embed=None)
+            return
+        
+        session = db.query(Session).filter(Session.id == sessionSelectView.session).first()
+        if session is None:
+            await msg.edit(content="Invalid session selected.", view=None, embed=None)
+            return
+    
+        groups = db.query(Group).filter(Group.owner_id == ctx.author.id, Group.session_id == session.id).all()
+        if len(groups) == 0:
+            await msg.edit(content="No groups found.", view=None, embed=None)
+            return
+        
+        groupSelectEmbed = discord.Embed(
+            title="Select a group",
+            description="Select a group.",
+            color=discord.Color.blue()
+        )
+        groupSelectView = GroupSelectView(groups)
+        await msg.edit(embed=groupSelectEmbed, view=groupSelectView, )
+        await groupSelectView.wait()
+        if groupSelectView.selected_group is None:
+            await msg.edit(content="No group selected.", view=None, embed=None)
+            return
+        
+        group = db.query(Group).filter(Group.id == groupSelectView.selected_group).first()
+        if group is None:
+            await msg.edit(content="Invalid group selected.", view=None, embed=None)
+            return
+        
+        groupJoin = db.query(GroupJoin).filter(GroupJoin.user_id == user.id, GroupJoin.group_id == group.id).first()
+        if groupJoin is None:
+            await msg.edit(content="User is not in this group.", view=None, embed=None)
+            return
+        
+        db.delete(groupJoin)
+        db.commit()
+        
+        successEmbed = discord.Embed(
+            title="User kicked",
+            description=f"{user.mention} has been kicked from group `{group.name}`.",
+            color=discord.Color.green()
+        )
+        await msg.edit(embed=successEmbed, view=None)
+        
+    @groups_command_group.command(name="invite", description="Invite a user to a group.")
+    async def group_invite(self, ctx, user: discord.Member):
+        if user == ctx.author:
+            await ctx.respond("You silly goose! You can't invite yourself to a group!", ephemeral=True)
+            return
+        sessionSelectEmbed = discord.Embed(
+            title="Select a session",
+            description="Select a session to pick a group for.",
+            color=discord.Color.blue()
+        )
+        sessionSelectView = SessionSelectView(ctx)
+        msg = await ctx.respond(embed=sessionSelectEmbed, view=sessionSelectView, ephemeral=True)
+        await sessionSelectView.wait()
+        if sessionSelectView.session is None:
+            await msg.edit(content="No session selected.", view=None, embed=None)
+            return
+        
+        session = db.query(Session).filter(Session.id == sessionSelectView.session).first()
+        if session is None:
+            await msg.edit(content="Invalid session selected.", view=None, embed=None)
+            return
+    
+        groups = db.query(Group).filter(Group.owner_id == ctx.author.id, Group.session_id == session.id).all()
+        if len(groups) == 0:
+            await msg.edit(content="No groups found.", view=None, embed=None)
+            return
+        
+        groupSelectEmbed = discord.Embed(
+            title="Select a group",
+            description="Select a group.",
+            color=discord.Color.blue()
+        )
+        groupSelectView = GroupSelectView(groups)
+        await msg.edit(embed=groupSelectEmbed, view=groupSelectView, )
+        await groupSelectView.wait()
+        if groupSelectView.selected_group is None:
+            await msg.edit(content="No group selected.", view=None, embed=None)
+            return
+        
+        group = db.query(Group).filter(Group.id == groupSelectView.selected_group).first()
+        if group is None:
+            await msg.edit(content="Invalid group selected.", view=None, embed=None)
+            return
+        
+        groupJoin = db.query(GroupJoin).filter(GroupJoin.user_id == user.id, GroupJoin.group_id == group.id).first()
+        if groupJoin is not None:
+            await msg.edit(content="User is already in this group.", view=None, embed=None)
+            return
+        
+        groupInvite = db.query(GroupJoin).filter(GroupJoin.user_id == user.id, GroupJoin.group_id == group.id, GroupJoin.is_invite == True).first()
+        if groupInvite is not None:
+            await msg.edit(content="User already has an invite to this group.", view=None, embed=None)
+            return
+        
+        groupJoin = GroupJoin(user_id=user.id, group_id=group.id, is_invite=True)
+        db.add(groupJoin)
+        db.commit()
+        
+        dm_channel = await user.create_dm()
+        
+        inviteEmbed = discord.Embed(
+            title=f"Group invite from {ctx.author.name}",
+            description=f"""
+            <@{ctx.author.id}> has invited you to join their group in {ctx.guild.name}.
+            To accept this invite, go to the server and join the group manually.
+            """,
+            color=discord.Color.blue()
+        )
+        await dm_channel.send(embed=inviteEmbed)        
+        
+        successEmbed = discord.Embed(
+            title="User invited",
+            description=f"{user.mention} has been invited to group `{group.name}`.",
+            color=discord.Color.green()
+        )
+        await msg.edit(embed=successEmbed, view=None)
+        
+    @groups_command_group.command(name="invites-list", description="Accept a group invite.")
+    async def group_accept(self, ctx):
+        invites_list = db.query(GroupJoin).filter(GroupJoin.user_id == ctx.author.id, GroupJoin.is_invite == True).all()
+        if len(invites_list) == 0:
+            await ctx.respond("No invites found.", ephemeral=True)
+            return
+        viewInvitesView = ViewInvitesView(invites_list)
+        viewInvitesEmbed = await viewInvitesView.update()
+        msg = await ctx.respond(embed=viewInvitesEmbed, view=viewInvitesView, ephemeral=True)
+        await viewInvitesView.wait()
+        if viewInvitesView.selected_invite is None:
+            await msg.edit(content="No invite selected.", view=None, embed=None)
+            return
+        
+        invite = db.query(GroupJoin).filter(GroupJoin.id == viewInvitesView.selected_invite).first()
+        if invite is None:
+            await msg.edit(content="Invalid invite selected.", view=None, embed=None)
+            return
+        
+        if viewInvitesView.action == "delete":
+            db.delete(invite)
+            group_name = db.query(Group).filter(Group.id == invite.group_id).first().name
+            successEmbed = discord.Embed(
+                title="Invite deleted",
+                description=f"Invite to {group_name} has been deleted.",
+                color=discord.Color.green()
+            )
+        
+        if viewInvitesView.action == "accept":        
+            invite.is_invite = False
+            db.add(invite)
+            group_name = db.query(Group).filter(Group.id == invite.group_id).first().name
+            successEmbed = discord.Embed(
+                title="Invite accepted",
+                description=f"You have accepted the invite to group `{group_name}`.",
+                color=discord.Color.green()
+            )
+        db.commit()
+        
+        
+        await msg.edit(embed=successEmbed, view=None)
+        
+    @groups_command_group.command(name='list', description="List groups.")
+    async def group_list(self, ctx):
+        sessionSelectEmbed = discord.Embed(
+            title="Select a session",
+            description="Select a session to list groups for.",
+            color=discord.Color.blue()
+        )
+        sessionSelectView = SessionSelectView(ctx)
+        msg = await ctx.respond(embed=sessionSelectEmbed, view=sessionSelectView, ephemeral=True)
+        await sessionSelectView.wait()
+        if sessionSelectView.session is None:
+            await msg.edit(content="No session selected.", view=None, embed=None)
+            return
+        
+        session = db.query(Session).filter(Session.id == sessionSelectView.session).first()
+        if session is None:
+            await msg.edit(content="Invalid session selected.", view=None, embed=None)
+            return
+        
+        groups = db.query(Group).filter(Group.session_id == session.id).all()
+        if len(groups) == 0:
+            await msg.edit(content="No groups found.", view=None, embed=None)
+            return
+        
+        groupListView = GroupsViewView(ctx=ctx, groups=groups)
+        groupListEmbed = await groupListView.update()
+        await msg.edit(embed=groupListEmbed, view=groupListView)
+        
+        
 def setup(bot):
     bot.add_cog(groupsCog(bot))
